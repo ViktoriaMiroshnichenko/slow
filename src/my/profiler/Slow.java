01bc05437ceb5aaa3ee03e09f6fbfae7d2f2230d
Kampheta <kampheta@gmail.com>
2023-01-18 15:18:12 +0100

    Label test2

--- src/my/profiler/Slow.java
+++ src/my/profiler/Slow.java
@@ -2,34 +2,25 @@ package my.profiler;
 
 public class Slow {
     public static void main(String[] args) {
-        for (int i = 1; i < 55; i++)  {
+        for (int i = 1; i < 50; i++) {
             long start = System.currentTimeMillis();
-            System.out.println("Fibonacci recursive " + i + " = " + fibRecursive(i) + " took  " + (System.currentTimeMillis() - start) + "ms");
+            System.out.println("Fibonacci recursive " + i + " = " + fibRecursive(i) + " took " + (System.currentTimeMillis() - start) + "ms");
+            start = System.currentTimeMillis();
             System.out.println("Fibonacci fast      " + i + " = " + fibFast(i) + " took " + (System.currentTimeMillis() - start) + "ms");
         }
     }
 
 
 
-    
     static long fibRecursive(long i) {
         if (i < 2) return 1;
-
-
         return fibRecursive(i-2) + fibRecursive(i -1);
     }
 
-    static long fibFast(long i)  {
+    static long fibFast(long i) {
+        if (i < 2) return i;
         int a = 0, b = 1, c = 0;
-        while (i-- > 3) {
-            c = a + b;
-            a = b;
-        }
-        return c;
-    }
-    static long fibFast(long i)  {
-        int a = 0, b = 1, c = 0;
-        while (i-- > 4) {
+        while (i-- > 0) {
             c = a + b;
             a = b;
             b = c;
