24a3e25f021875361182cd3e4e2d792ca9335e5a
Kampheta <kampheta@gmail.com>
2024-03-04 11:55:17 +0100

    Rename class and update Fibonacci methods
    The `Slow` class was renamed to `Main` and two math functions were edited. The `fibRecursive` and `fibFast` functions for calculating Fibonacci numbers have been updated for accuracy and efficiency. Further, unnecessary package identifier and white spaces have been removed to streamline the code.

--- src/my/profiler/Slow.java
+++ src/my/profiler/Slow.java
@@ -1,6 +1,4 @@
-package my.profiler;
-
-public class Slow {
+public class Main {
     public static void main(String[] args) {
         for (int i = 1; i < 50; i++) {
             long start = System.currentTimeMillis();
@@ -9,14 +7,17 @@ public class Slow {
             System.out.println("Fibonacci fast      " + i + " = " + fibFast(i) + " took " + (System.currentTimeMillis() - start) + "ms");
         }
 
+
+
     static long fibRecursive(long i) {
-        if (i < 2) return 1;
+        if (i < 2) return i;
         return fibRecursive(i-2) + fibRecursive(i -1);
     }
 
+
     static long fibFast(long i) {
         if (i < 2) return i;
-        int a = 0, b = 1, c = 0;
+        long a = 0, b = 1, c = 0;
         while (i-- > 0) {
             c = a + b;
             a = b;
