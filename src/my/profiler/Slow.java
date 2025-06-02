ab537cf3ce7e5f212139e54f429ae591970c3e63
Kampheta <kampheta@gmail.com>
2025-06-02 18:48:37 +0200

    Adjust Fibonacci logic and update miscellaneous text content.

--- src/my/profiler/Slow.java
+++ src/my/profiler/Slow.java
@@ -20,7 +20,7 @@ public class Slow {
     static long fibFast(long i) {
         if (i < 2) return i;
         int a = 0, b = 1, c = 0;
-        while (i-- > 4) {
+        while (i-- > 0) {
             c = a + b;
             a = b;
             b = c;
