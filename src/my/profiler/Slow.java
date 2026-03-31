c5c4fefdacea7ab6cccf81c4baf0ba5cc65fd7a0
Kampheta <kampheta@gmail.com>
2026-03-31 17:56:18 +0200

    test

--- src/my/profiler/Slow.java
+++ src/my/profiler/Slow.java
@@ -1,33 +1,21 @@
 package my.profiler;
 
 public class Slow {
-    public static void main(String[] args) {
-        for (int i = 1; i < 50; i++) {
-            long start = System.currentTimeMillis();
-            System.out.println("Fibonacci recursive " + i + " = " + fibRecursive(i) + " took " + (System.currentTimeMillis() - start) + "ms");
-            start = System.currentTimeMillis();
-            System.out.println("Fibonacci fast      " + i + " = " + fibFast(i) + " took " + (System.currentTimeMillis() - start) + "ms");
+    private static volatile long sink;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int round = 0; round < 50; round++) {
+            for (int i = 0; i < 5; i++) {
+                sink = fibRecursive(39);
+            }
+            Thread.sleep(50);
         }
+
+        System.out.println("Done: " + sink);
     }
 
-
-
-    static long fibRecursive(long i) {
-        if (i < 2) return 1;
-        return fibRecursive(i-2) + fibRecursive(i -1);
-    }
-
-
-
-
-    static long fibFast(long i) {
-        if (i < 2) return i;
-        int a = 0, b = 1, c = 0;
-        while (i-- > 0) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return c;
+    static long fibRecursive(long n) {
+        if (n < 2) return 1;
+        return fibRecursive(n - 2) + fibRecursive(n - 1);
     }
 }
